b00d09acf08e52dd69972d27835d97f8bc404b41
Alkaid <zgf574564920@gmail.com>
2021-05-04 18:46:48 -0400

    Synthesize and perform depth-sweep on all xilinx bugs
    To make them synthesizable in vivado, each bug is equipped with
    1. A tcl script to mimic the process of creating, synthesizing and
    report utilizations in the vivado
    2. A Verilog wrapper to contain the systemverilog code generated from
    our tools. Note that except AXI-Lite-DropAck and
    AXI-Stream-Incorrect-Last, the wrappers of all other bugs have a
    AXI-Lite interface "unnaturally" wired to each application. Those
    wrappers do not comply AXI-Lite protocol and only created for synthesis.
    
    The list of Xilinx bugs included in this commit:
    - AXI-Lite-DropAck
    - AXI-Stream-Incorrect-Last
    - SDSPI-path-merging
    - SDSPI-deadlock
    - SDSPI-Endian
    - FFT-BitTruncation
    - FADD-Misindexing

--- dblclockfft-integer-overflow/vivado_synth/fft_wrapper.v
+++ dblclockfft-integer-overflow/vivado_synth/fft_wrapper.v
@@ -1,4 +1,4 @@
-module xlnxdemo_wrapper (
+module fft_wrapper (
   S_AXI_ACLK,
   S_AXI_ARESETN,
   S_AXI_AWADDR,
@@ -44,30 +44,14 @@ output wire [1 : 0] S_AXI_RRESP;
 output wire S_AXI_RVALID;
 input wire S_AXI_RREADY;
 
-  xlnxdemo
-  //#( .C_S_AXI_DATA_WIDTH(32), .C_S_AXI_ADDR_WIDTH(7))
+  fftmain
   inst (
-    .S_AXI_ACLK(S_AXI_ACLK),
-    .S_AXI_ARESETN(S_AXI_ARESETN),
-    .S_AXI_AWADDR(S_AXI_AWADDR),
-    .S_AXI_AWPROT(S_AXI_AWPROT),
-    .S_AXI_AWVALID(S_AXI_AWVALID),
-    .S_AXI_AWREADY(S_AXI_AWREADY),
-    .S_AXI_WDATA(S_AXI_WDATA),
-    .S_AXI_WSTRB(S_AXI_WSTRB),
-    .S_AXI_WVALID(S_AXI_WVALID),
-    .S_AXI_WREADY(S_AXI_WREADY),
-    .S_AXI_BRESP(S_AXI_BRESP),
-    .S_AXI_BVALID(S_AXI_BVALID),
-    .S_AXI_BREADY(S_AXI_BREADY),
-    .S_AXI_ARADDR(S_AXI_ARADDR),
-    .S_AXI_ARPROT(S_AXI_ARPROT),
-    .S_AXI_ARVALID(S_AXI_ARVALID),
-    .S_AXI_ARREADY(S_AXI_ARREADY),
-    .S_AXI_RDATA(S_AXI_RDATA),
-    .S_AXI_RRESP(S_AXI_RRESP),
-    .S_AXI_RVALID(S_AXI_RVALID),
-    .S_AXI_RREADY(S_AXI_RREADY)
+    .i_clk(S_AXI_ACLK),
+    .i_reset(!S_AXI_ARESETN),
+    .i_ce(S_AXI_AWVALID),
+    .i_sample(S_AXI_WDATA),
+    .o_result({S_AXI_WREADY, S_AXI_BRESP, S_AXI_BVALID, S_AXI_ARREADY, S_AXI_RDATA, S_AXI_RRESP, S_AXI_RVALID}),
+    .o_sync(S_AXI_AWREADY)
   );
 endmodule
 
