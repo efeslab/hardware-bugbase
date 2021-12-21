876b8d4f4624b6d085a11c5d54526353e459d058
Haoyang Zhang <haoyoung@cilantro.eecs.umich.edu>
2021-12-21 15:45:09 -0500

    Rename by N

---
+++ verilog-axis-fail-to-reset-N1/test/main.cpp
@@ -0,0 +1,76 @@
+#include <stdlib.h>
+#include <iostream>
+#include <stdio.h>
+#include <cassert>
+#include <verilated_fst_c.h>
+#include <verilated.h>
+#include <queue>
+#include <map>
+#include <tuple>
+#include <exception>
+#include <csetjmp>
+#include <csignal>
+
+#include "Vtest_axis_frame_len_8.h"
+
+using namespace std;
+
+
+jmp_buf jmp_env;
+void sig_handler(int signum) {
+    longjmp(jmp_env, 1);
+}
+
+const uint64_t psc = 2500; // 2500 ps per cycle
+const uint64_t pst = 1250; // 1250 ps per tick (half cycle)
+uint64_t timestamp = 0;
+double sc_time_stamp() {
+    return timestamp;
+}
+void sc_time_step() {
+    timestamp += pst;
+}
+
+int main(int argc, char **argv) {
+    
+
+    Verilated::commandArgs(argc, argv);
+    Vtest_axis_frame_len_8 *tb = new Vtest_axis_frame_len_8;
+    
+    Verilated::traceEverOn(true);
+    VerilatedFstC *trace = new VerilatedFstC;
+    tb->trace(trace, 99);
+    string waveform = string(argv[0])+".fst";
+    trace->open(waveform.c_str());
+    
+    
+    if (setjmp(jmp_env) == 0) {
+        signal(SIGABRT, &sig_handler);
+        signal(SIGINT, &sig_handler);
+    } else {
+        goto save_trace_and_exit;
+    }
+   
+    while (!Verilated::gotFinish()) {
+        tb->clk = 1;
+        tb->eval();
+        trace->dump(timestamp);
+        sc_time_step();
+
+        tb->clk = 0;
+        tb->eval();
+        trace->dump(timestamp);
+        sc_time_step();
+    }
+    delete tb;
+
+
+save_trace_and_exit:
+
+    trace->flush();
+    trace->close();
+
+    exit(EXIT_SUCCESS);
+    
+
+}
